9194ed3532edd5694c4739c7a20cfa6b9d3828df
Brian Wakenshaw <bri.wakey@hotmail.co.uk>
2018-02-20 07:56:35 +0000

    changed value names

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,14 +1,13 @@
 object Partridge {
 
-
   val terms = List("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
-  val some = "Mine's a Pint"
-  val none = "Lynn, I've pierced my foot on a spike!!"
+  val foundSome = "Mine's a Pint"
+  val foundNone = "Lynn, I've pierced my foot on a spike!!"
 
   def part(args : Array[String]): String = {
     args.count(term => terms.contains(term)) match {
-      case 0 => none
-      case x => some + "!" * x
+      case 0 => foundNone
+      case x => foundSome + "!" * x
 
     }
   }
